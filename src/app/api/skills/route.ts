fdeb248939694cde33fbf8b5b5cc04c9a8b02b13
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-09-30 17:33:51 -0600

    Created Test - API Handler

---
+++ src/app/api/skills/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from "next/server";
+import SkillsControllers from "../../lib/controllers/SkillsController";
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+
+  const validParams = ["name", "id"];
+  const invalidParams: string[] = [];
+
+  searchParams.forEach((_, key) => {
+    if (!validParams.includes(key)) {
+      invalidParams.push(key);
+    }
+  });
+
+  if (invalidParams.length > 0) {
+    return NextResponse.json(
+      {
+        message: `Invalid query parameters: ${invalidParams.join(", ")}`,
+      },
+      { status: 400 },
+    );
+  }
+
+  const name = searchParams.get("name");
+  if (name) {
+    const response = await SkillsControllers.getSkillsByName(name);
+    return NextResponse.json(response.body, { status: response.status });
+  }
+
+  const id = searchParams.get("id");
+  if (id) {
+    const response = await SkillsControllers.getSkillsByID(id);
+    return NextResponse.json(response.body, { status: response.status });
+  }
+
+  const response = await SkillsControllers.getAllSkills();
+  return NextResponse.json(response.body, { status: response.status });
+}

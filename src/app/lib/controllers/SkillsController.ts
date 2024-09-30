348ac682ea4e84b8e3a838966aa7212a6a650e19
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-09-30 17:32:48 -0600

    Created Skills - Controller

---
+++ src/app/lib/controllers/SkillsController.ts
@@ -0,0 +1,72 @@
+import SkillsService from "../services/SkillsService";
+import SkillsRepository from "../repositories/SkillsRepository";
+import Skills from "../entities/Skills";
+import ResponseData from "../constants/api/ResponseData";
+const skillsRepository = new SkillsRepository();
+const skillsService = new SkillsService(skillsRepository);
+
+export const getAllSkills = async (): Promise<
+  ResponseData<Skills[]> | ResponseData<string>
+> => {
+  try {
+    const skills = await skillsService.getAllSkills();
+    return new ResponseData(200, skills);
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(500, "Failed to retrieve skills");
+  }
+};
+
+export const getSkillsByName = async (
+  name: string | null,
+): Promise<ResponseData<Skills | null> | ResponseData<string>> => {
+  if (!name || typeof name !== "string") {
+    return new ResponseData(400, "Name is required and should be a string.");
+  }
+
+  try {
+    const skill = await skillsService.getSkillsByName(name);
+    if (skill) {
+      return new ResponseData(200, skill);
+    } else {
+      return new ResponseData(200, "No Skill fetched with name: " + name);
+    }
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(
+      500,
+      "Failed to retrieve skill with name. Name: " + name + " is not defined",
+    );
+  }
+};
+
+export const getSkillsByID = async (
+  id: string | null,
+): Promise<ResponseData<Skills | null> | ResponseData<string>> => {
+  if (!id || typeof id !== "string") {
+    return new ResponseData(400, "ID is required and should be a string.");
+  }
+
+  try {
+    const skill = await skillsService.getSkillsByID(id);
+    if (skill) {
+      return new ResponseData(200, skill);
+    } else {
+      return new ResponseData(200, "No Skill fetched with ID: " + id);
+    }
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(
+      500,
+      "Failed to retrieve skill with ID. ID: " + id + " is not defined",
+    );
+  }
+};
+
+const SkillsController = {
+  getAllSkills,
+  getSkillsByName,
+  getSkillsByID,
+};
+
+export default SkillsController;

271c46702782779656639d95f121011f860df597
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-09-30 17:32:12 -0600

    Created Skills - Enumerations

---
+++ src/app/lib/constants/enumerations/KnowledgeLevelsEnumerations.ts
@@ -0,0 +1,5 @@
+export enum KnowledgeLevelEnumerations {
+  Low = "Basic understanding of coding concepts",
+  Mid = "Proficient in intermediate coding skills",
+  High = "Advanced proficiency in coding",
+}

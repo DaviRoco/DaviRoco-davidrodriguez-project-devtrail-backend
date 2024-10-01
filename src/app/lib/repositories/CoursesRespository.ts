4ef148496cea0e0955ce3fd32b1f9bd7544173ed
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-10-01 13:34:40 -0600

    Created Courses - Repository

---
+++ src/app/lib/repositories/CoursesRespository.ts
@@ -0,0 +1,123 @@
+import { db } from "../../firebase";
+import {
+  collection,
+  getDocs,
+  doc,
+  getDoc,
+  where,
+  query,
+} from "firebase/firestore";
+import Courses from "../entities/Courses";
+
+const coursesCollection = collection(db, "courses");
+
+export class CoursesRepository {
+  async getAllCourses(): Promise<Courses[]> {
+    const coursesSnapshot = await getDocs(coursesCollection);
+    const courses = coursesSnapshot.docs.map((doc) => {
+      const data = doc.data();
+
+      if (
+        !data.name ||
+        !data.code ||
+        !data.description ||
+        !data.institution ||
+        !data.skills
+      ) {
+        throw new Error(
+          `Course with ID ${doc.id} is missing mandatory fields.`,
+        );
+      }
+
+      return new Courses(
+        doc.id,
+        data.name,
+        data.code,
+        data.description,
+        data.institution,
+        data.skills,
+      );
+    });
+
+    return courses;
+  }
+
+  async getCoursesByName(name: string): Promise<Courses | null> {
+    if (!name || typeof name !== "string") {
+      throw new Error(
+        "Invalid name provided. Name must be a non-empty string.",
+      );
+    }
+
+    const courseMatchingName = query(
+      coursesCollection,
+      where("name", "==", name),
+    );
+
+    const querySnapshot = await getDocs(courseMatchingName);
+
+    if (querySnapshot.empty) {
+      return null;
+    }
+
+    const docSnapshot = querySnapshot.docs[0];
+    const docData = docSnapshot.data();
+
+    if (
+      !docData.name ||
+      !docData.code ||
+      !docData.description ||
+      !docData.institution ||
+      !docData.skills
+    ) {
+      throw new Error(`Course with name ${name} is missing mandatory fields.`);
+    }
+
+    const course = new Courses(
+      docSnapshot.id,
+      docData.name,
+      docData.code,
+      docData.description,
+      docData.institution,
+      docData.skills,
+    );
+
+    return course;
+  }
+
+  async getCoursesByID(id: string): Promise<Courses | null> {
+    if (!id || typeof id !== "string") {
+      throw new Error("Invalid ID provided. ID must be a non-empty string.");
+    }
+
+    const courseDoc = doc(coursesCollection, id);
+    const docSnapshot = await getDoc(courseDoc);
+
+    if (!docSnapshot.exists()) {
+      return null;
+    }
+
+    const docData = docSnapshot.data();
+
+    if (
+      !docData.name ||
+      !docData.code ||
+      !docData.description ||
+      !docData.institution ||
+      !docData.skills
+    ) {
+      throw new Error(`Course with ID ${id} is missing mandatory fields.`);
+    }
+
+    const course = new Courses(
+      docSnapshot.id,
+      docData.name,
+      docData.code,
+      docData.description,
+      docData.institution,
+      docData.skills,
+    );
+
+    return course;
+  }
+}

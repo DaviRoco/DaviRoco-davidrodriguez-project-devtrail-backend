2194bfbff19916937512bf95b6c26d9df88d59af
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-09-30 21:33:52 -0600

    Created Projects - Controller

---
+++ src/app/lib/controllers/ProjectsController.ts
@@ -0,0 +1,73 @@
+import { ProjectsService } from "../services/ProjectsService";
+import ProjectsRepository from "../repositories/ProjectsRepository";
+import ResponseData from "../constants/api/ResponseData";
+import Projects from "../entities/Projects";
+
+const projectsRepository = new ProjectsRepository();
+const projectsService = new ProjectsService(projectsRepository);
+
+export const getAllProjects = async (): Promise<
+  ResponseData<Projects[]> | ResponseData<string>
+> => {
+  try {
+    const projects = await projectsService.getAllProjects();
+    return new ResponseData(200, projects);
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(500, "Failed to retrieve projects");
+  }
+};
+
+export const getProjectsByName = async (
+  name: string | null,
+): Promise<ResponseData<Projects | null> | ResponseData<string>> => {
+  if (!name || typeof name !== "string") {
+    return new ResponseData(400, "Name is required and should be a string.");
+  }
+
+  try {
+    const project = await projectsService.getProjectsByName(name);
+    if (project) {
+      return new ResponseData(200, project);
+    } else {
+      return new ResponseData(200, "No Project fetched with name: " + name);
+    }
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(
+      500,
+      "Failed to retrieve project with name. Name: " + name + " is not defined",
+    );
+  }
+};
+
+export const getProjectsByID = async (
+  id: string | null,
+): Promise<ResponseData<Projects | null> | ResponseData<string>> => {
+  if (!id || typeof id !== "string") {
+    return new ResponseData(400, "ID is required and should be a string.");
+  }
+
+  try {
+    const project = await projectsService.getProjectsByID(id);
+    if (project) {
+      return new ResponseData(200, project);
+    } else {
+      return new ResponseData(200, "No Project fetched with ID: " + id);
+    }
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(
+      500,
+      "Failed to retrieve project with ID. ID: " + id + " is not defined",
+    );
+  }
+};
+
+const ProjectsController = {
+  getAllProjects,
+  getProjectsByName,
+  getProjectsByID,
+};
+
+export default ProjectsController;

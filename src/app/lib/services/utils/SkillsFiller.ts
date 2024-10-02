eba7263a0d8f79a9412cfda7b65231c397f89e88
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-10-01 19:18:24 -0600

    Created Skills Filler - Util
    This util is used to get the matching skills with the specified ID and return the object with them.

---
+++ src/app/lib/services/utils/SkillsFiller.ts
@@ -0,0 +1,57 @@
+import Skills from "../../entities/Skills";
+import SkillsRepository from "../../repositories/SkillsRepository";
+
+interface HasSkills {
+  skills: Skills[];
+}
+
+class SkillsFiller<T extends HasSkills> {
+  private skillsRepository: SkillsRepository;
+
+  constructor() {
+    this.skillsRepository = new SkillsRepository();
+  }
+
+  public async getObjectWithSkills(
+    objectData: T | null,
+  ): Promise<Omit<T, "skills"> | null> {
+    if (objectData) {
+      const skillIds = objectData.skills as unknown as Skills[];
+      const skillsArray = await this.skillsRepository.getSkillsByID(skillIds);
+      const result = {
+        ...objectData,
+        _skills: skillsArray,
+      } as T;
+      return result;
+    }
+
+    return null;
+  }
+
+  public async getObjectsWithSkills(
+    objects: T[] | null,
+  ): Promise<Omit<T, "skills">[] | null> {
+    if (objects?.length) {
+      const objectResult: Omit<T, "skills">[] = await Promise.all(
+        objects.map(async (object) => {
+          const skillIds = object.skills;
+          const skillsArray =
+            await this.skillsRepository.getSkillsByID(skillIds);
+
+          const result = {
+            ...object,
+            _skills: skillsArray,
+          };
+
+          return result;
+        }),
+      );
+
+      return objectResult;
+    }
+
+    return null;
+  }
+}
+
+export default SkillsFiller;

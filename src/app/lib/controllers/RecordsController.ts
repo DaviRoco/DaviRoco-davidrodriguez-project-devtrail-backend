ba3d83e3e77ed0e361f3e17a941ff5389943ed51
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-10-02 18:59:10 -0600

    Created Records - Controller

---
+++ src/app/lib/controllers/RecordsController.ts
@@ -0,0 +1,110 @@
+import { RecordsService } from "../services/RecordsService";
+import { RecordsRepository } from "../repositories/RecordsRepository";
+import ResponseData from "../constants/api/ResponseData";
+import ExperienceRecords from "../entities/ExperienceRecords";
+import EducationalRecords from "../entities/EducationalRecords";
+
+export const getAllExperienceRecords = async (): Promise<
+  ResponseData<ExperienceRecords[]> | ResponseData<string>
+> => {
+  try {
+    const recordsRepository = new RecordsRepository("experience");
+    const recordsService = new RecordsService(recordsRepository, "experience");
+    const records =
+      (await recordsService.getAllExperienceRecords()) as ExperienceRecords[];
+    if (!records) {
+      return new ResponseData(200, "No Experience records fetched");
+    }
+    return new ResponseData(200, records);
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(500, "Failed to retrieve experience records");
+  }
+};
+
+export const getAllEducationalRecords = async (): Promise<
+  ResponseData<EducationalRecords[]> | ResponseData<string>
+> => {
+  try {
+    const recordsRepository = new RecordsRepository("education");
+    const recordsService = new RecordsService(recordsRepository, "education");
+    const records =
+      (await recordsService.getAllEducationalRecords()) as EducationalRecords[];
+    if (!records) {
+      return new ResponseData(200, "No Educational records fetched");
+    }
+    return new ResponseData(200, records);
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(500, "Failed to retrieve educational records");
+  }
+};
+
+export const getExperienceRecordByID = async (
+  id: string | null,
+): Promise<ResponseData<ExperienceRecords | null> | ResponseData<string>> => {
+  if (!id || typeof id !== "string") {
+    return new ResponseData(400, "ID is required and should be a string.");
+  }
+
+  try {
+    const recordsRepository = new RecordsRepository("experience");
+    const recordsService = new RecordsService(recordsRepository, "experience");
+    const record = (await recordsService.getExperienceRecordByID(
+      id,
+    )) as ExperienceRecords;
+    if (record) {
+      return new ResponseData(200, record);
+    } else {
+      return new ResponseData(
+        200,
+        "No Experience record fetched with ID: " + id,
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(
+      500,
+      "Failed to retrieve experience record with ID: " + id,
+    );
+  }
+};
+
+export const getEducationalRecordByID = async (
+  id: string | null,
+): Promise<ResponseData<EducationalRecords | null> | ResponseData<string>> => {
+  if (!id || typeof id !== "string") {
+    return new ResponseData(400, "ID is required and should be a string.");
+  }
+
+  try {
+    const recordsRepository = new RecordsRepository("education");
+    const recordsService = new RecordsService(recordsRepository, "education");
+    const record = (await recordsService.getEducationalRecordByID(
+      id,
+    )) as EducationalRecords;
+    if (record) {
+      return new ResponseData(200, record);
+    } else {
+      return new ResponseData(
+        200,
+        "No Educational record fetched with ID: " + id,
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return new ResponseData(
+      500,
+      "Failed to retrieve educational record with ID: " + id,
+    );
+  }
+};
+
+const RecordsController = {
+  getAllExperienceRecords,
+  getAllEducationalRecords,
+  getExperienceRecordByID,
+  getEducationalRecordByID,
+};
+
+export default RecordsController;

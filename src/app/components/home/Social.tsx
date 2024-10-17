755c348bfab09eef69be4f9a8c05b616e298fab9
David Rodríguez Coto <64442948+DaviRoco@users.noreply.github.com>
2024-10-17 15:52:17 -0600

    Created Home - UI Component

---
+++ src/app/components/home/Social.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Social = () => {
+  return (
+    <div className="home-social">
+      <a
+        href="https://www.linkedin.com/in/david-rodriguez-coto"
+        className="home-social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/DaviRoco"
+        className="home-social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from 'next/image';
 import styles from './page.module.css';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
-export default function Home() {
+export default function App() {
   return (
     <div className={styles.page}>
       <Header />
+      <main className="main">
+        <Home />
+      </main>
     </div>
   );
 }
